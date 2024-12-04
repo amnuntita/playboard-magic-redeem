20dfa23840996bdc623266b5f25b1825adf842ff
nuntita <nuntita@inox.co.th>
2024-12-04 12:02:03 +0700

    feat: get domain, slug, token from input

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [domain, setDomain] = useState(
+    "https://www.playboard-develop-1.poring.arkcube.com"
+  );
+  const [brandSlug, setBrandSlug] = useState("some-event-brand");
+  const [inputValue, setInputValue] = useState("");
 
-  const [jwtPayload, setJWTPayload] = useState({
-    userRefCode: 'user-id-1234',
-    userDisplayName: 'John Doe',
-    redeemCode: 'event123'
-  })
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-  function signToken(payload){
-    const signedToken = jwt.sign(payload, process.env.REACT_APP_PUBLIC_KEY)
-    const rerunLink = process.env.REACT_APP_BASE_URL+signedToken
-    return rerunLink 
-  }
+  const getLink = () => domain + "/" + brandSlug + "/magic-redeem?token=" + inputValue;
 
   return (
     <div className="App">
-      <header>
-        <h2>Ticket Platform</h2>
-        
-        <button>
-          <a href={signToken(jwtPayload)} target='_blank'>รับชม Rerun Event</a>
-        </button>
-      </header>
+      <h2>Ticket Platform</h2>
+      <div>
+        Domain:{" "}
+        <input
+          value={domain}
+          handleChange={(e) => setDomain(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        Your brand slug:{" "}
+        <input
+          value={brandSlug}
+          handleChange={(e) => setBrandSlug(e.target.value)}
+        ></input>
+      </div>
+      <p>
+        Input your signed token here Ref:{" "}
+        <a href="https://dinochiesa.github.io/jwt/">JWT Generator</a>
+      </p>
+      <textarea rows={10} value={inputValue} onChange={handleChange} />
+      <button>
+        <a href={getLink()} target="_blank" rel="noreferrer">
+          รับชม Rerun Event
+        </a>
+      </button>
     </div>
   );
 }
